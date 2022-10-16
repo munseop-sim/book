31ae6d4cb1772728f9be145b3334a65aba371dd9
munseop.sim.mac <ms2709@a2dcorp.co.kr>
2022-10-16 22:29:45 +0900

    PersonalPlaylist 클래스 생성
    플레이리스트에서 삭제 기능이 추가된 PersonalPlaylist(Playlist를 상속받음) 클래스 작성

--- chapter10/kotlin/SongAndPlaylist.kt
+++ chapter10/kotlin/SongAndPlaylist.kt
@@ -11,9 +11,19 @@ class Song(private val singer:String,private val title:String){
 /**
  * 초기 요구사항으로 인해 작성된 코드
  */
-class Playlist(){
+open class Playlist(){
     private val tracks:ArrayList<Song> = ArrayList()
 
     fun getTracks():List<Song> = this.tracks
     fun append(song:Song) = this.tracks.add(song)
+}
+
+
+/**
+ * 플레이스트에서 삭제 기능이 추가된 PersonalPlaylist 클래스
+ */
+class PersonalPlaylist(): Playlist() {
+    fun remove(song:Song){
+        (getTracks() as ArrayList).remove(song)
+    }
 }
