e1187118a424a4b17983b3fcece04609ba5814d3
munseop-sim <76484177+munseop-sim@users.noreply.github.com>
2023-03-20 22:17:05 +0900

    Delete py_code directory

--- py_code/Util.py
+++
@@ -1,4 +0,0 @@
-class Util:
-    @staticmethod
-    def get_dict_value(dict_val, key_name):
-        return dict_val[key_name] if dict_val.keys().__contains__(key_name) else None
--- py_code/ch01/Invitation.py
+++
@@ -1,6 +0,0 @@
-from datetime import datetime
-
-# 초대장 구현
-class Invitation:
-    def __init__(self):
-        self._when: datetime = None # 관람일자
